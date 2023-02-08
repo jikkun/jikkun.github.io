857d04448ad39be1263c124f953916fd9746baf7
jikkun <henjuck@gmail.com>
2023-02-08 16:33:58 +0900

    a

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[Jekyll] 블로그 포스팅하는 방법",
+        "excerpt":"1. 1번  마크다운으로 작성   2. 2번 자바코드 테스트  public class Config {     byte[] a1;     final Map&lt;String, Object&gt; b1 = new HashMap&lt;String, Object&gt;();     String c1; }   마무리  끝   ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/%EC%B2%AB-%ED%85%8C%EC%8A%A4%ED%8A%B8-%EA%B8%80/",
+        "teaser": null
+      }]
